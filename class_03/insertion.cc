629fa1994fc087dd35f38a9dfb77ece99439ec27
aoikida <t20249ak@sfc.keio.ac.jp>
2022-10-21 15:43:43 +0900

    Change some

--- class_03/insertion.cc
+++ class_03/insertion.cc
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <sys/time.h>
 using namespace std;
+
+#define NUM_DATA 100000
 
 void insertion_sort(vector<int> &A, int size){
     int i, j, key;
@@ -13,22 +17,34 @@ void insertion_sort(vector<int> &A, int size){
             i = i-1;
         }
         A[i + 1] = key;
-        for (i = 0; i < size; ++i) cout << A[i] << endl;
     }
-
 }
 
 int main(){
 
-    vector<int> array = {5, 4, 3, 2, 1};
+    int i;
+    unsigned int sec;
+    clock_t start_clock, end_clock;
 
-    int N = array.size();
+    vector<int> array(NUM_DATA);
+
+    for(i = 0; i<NUM_DATA; i++){
+        array[i] = NUM_DATA - i;
+    }
+
+    start_clock = clock();
 
     //挿入ソート
-    insertion_sort(array, N);
+    insertion_sort(array, NUM_DATA);
+
+    end_clock = clock();
 
     //出力
-    for (int i = 0; i < N; ++i) cout << array[i] << endl;
-    
+    for (i = 0; i < NUM_DATA; ++i) {
+        cout << array[i] << ",";
+    }
+    cout << endl;
 
+    printf("clock:%f\n", (double)(end_clock - start_clock) / CLOCKS_PER_SEC);
+    
 }
--- class_03/quick.cc
+++ class_03/quick.cc
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-#define NUM_ITEM 5
+#define NUM_ITEM 100000
 
 
 void quick_sort(vector<int> &array, int left, int right){
@@ -28,24 +28,31 @@ void quick_sort(vector<int> &array, int left, int right){
 }
 
 int main(){
+  int i;
+  unsigned int sec;
+  clock_t start_clock, end_clock;
 
   //入力
   vector<int> input_datas(NUM_ITEM);
 
   //入力データを作成
-  for (int i = 0; i < 5; i++){
+  for (int i = 0; i < NUM_ITEM; i++){
     input_datas[i] = NUM_ITEM - i;
-    cout << input_datas[i];
   }
-  cout << endl;
+
+  start_clock = clock();
 
   //クイックソート
   quick_sort(input_datas, 0, NUM_ITEM);
 
+  end_clock = clock();
+
   //出力
   for(int i = 0; i<NUM_ITEM; i++){
-    cout << input_datas[i];
+    cout << input_datas[i] << ", ";
   }
   cout << endl;
+
+  printf("clock:%f\n", (double)(end_clock - start_clock) / CLOCKS_PER_SEC);
 
 }
