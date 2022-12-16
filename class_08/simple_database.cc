ee0b6168f1ebfffd699bfed4379ef765efaa2508
aoikida <t20249ak@sfc.keio.ac.jp>
2022-12-16 23:25:06 +0900

    Add class_08

---
+++ class_08/simple_database.cc
@@ -0,0 +1,135 @@
+#include "bptree.h"
+
+#include <vector>
+#include <random>
+
+
+#define DATA_NUMBER 100000
+#define NUM_THREAD 8
+#define NUM_TRANSACTION 100000
+#define NUM_OPERATION 16
+
+std::vector<DATA> record_set(DATA_NUMBER);
+
+
+void
+worker() {
+	
+	for(int i = 0; i < NUM_TRANSACTION; ++i){
+		std::random_device rnd;    
+		std::mt19937 mt(rnd());     
+		std::uniform_int_distribution<> random(0, DATA_NUMBER-1);       
+		RETRY :
+		for(int j = 0; j < NUM_OPERATION; ++j){
+		DATA * record = &record_set[random(mt)];
+		if (record->rwlock.r_trylock()) {
+			if (search_key(Root, record->key) == 1) {
+				cout << record->key << "はデータベースにありません" << endl;
+			}
+		}
+		else { 
+			goto RETRY;
+		}
+    }
+	}
+
+	cout << "finish read-only transaction" << endl;
+}
+
+int
+main(int argc, char *argv[])
+{
+	
+	unsigned int i;
+    unsigned int sec;
+    int nsec;
+    double d_sec;
+    struct timespec start_time, end_time;
+
+	init_root();
+
+	//make database
+	for(i = 0; i < DATA_NUMBER; i++){
+		(&record_set[i])->key = i+1;
+		(&record_set[i])->val = 1;
+		(&record_set[i])->rwlock.init();
+		(&record_set[i])->next = &record_set[i+1];
+	}
+
+	printf("-----Insert-----\n");
+	
+	for(i = 0; i < DATA_NUMBER; i++){
+		insert((&record_set[i])->key, &record_set[i]);
+	}
+
+	print_tree(Root);
+
+	/*
+	std::vector<std::thread> worker_threads;
+
+	
+    for (size_t i = 0; i < NUM_THREAD; ++i) {
+        threads.push_back(worker);
+    }
+
+	for(size_t i = 0; i < NUM_THREAD; ++i) {
+		threads[i].join();
+	}
+	*/
+
+	clock_gettime(CLOCK_REALTIME, &start_time);
+
+	std::vector<std::thread> threads;
+
+	for (size_t i = 0; i < NUM_THREAD; ++i) {
+        threads.emplace_back(std::thread(worker));
+    }
+
+	for(auto& thread : threads){
+        thread.join();
+    }
+
+	clock_gettime(CLOCK_REALTIME, &end_time);
+
+	sec = end_time.tv_sec - start_time.tv_sec;
+    nsec = end_time.tv_nsec - start_time.tv_nsec;
+
+    d_sec = (double)sec
+        + (double)nsec / (1000 * 1000 * 1000);
+
+    printf("time:%f\n", d_sec);
+	printf("throughput:%f\n", (NUM_TRANSACTION * NUM_THREAD)/d_sec);
+
+	/*
+	
+	printf("-----Search-----\n");
+
+	for(i = 0; i < DATA_NUMBER; i++){
+		search_key(Root, (&record_set[i])->key);
+	}
+
+	printf("%d\n", count);
+
+	
+
+	printf("------UPDATE------\n");
+	//before UPDATE
+	for(i = 0; i < DATA_NUMBER; i++){
+		cout << (&record_set[i])->val;
+	}
+	cout << endl; 
+	//UPDATE
+	for(i = 0; i < DATA_NUMBER; i++){
+		update(Root, &record_set[i]);
+	}
+	//after UPDATE
+	for(i = 0; i < DATA_NUMBER; i++){
+		cout << (&record_set[i])->val;
+	}
+	cout << endl;
+
+	*/
+
+  	return 0;
+
+}
